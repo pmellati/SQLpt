a332a85b4614c94407c3a3ca31d26099c976c55e
Pouria Mellati <pouria@pouria.me>
2016-09-11 23:03:08 +1000

    Redesigned column algebra where column types (and therefore available operations) are decoupled from column construction details.

---
+++ src/main/scala/sqlpt/columndesign1/Column.scala
@@ -0,0 +1,57 @@
+package sqlpt.columndesign1
+
+
+sealed trait Column[+T <: Column.Type]
+
+object Column {
+  sealed trait Type
+
+  object Type {
+    case object Str extends Type
+    case object Num extends Type    // We support the same ops for all numeric types, so they are all represented by `Num`.
+
+    type Str = Str.type
+    type Num = Num.type
+  }
+
+  import Type._
+
+  case class SourceColumn[T <: Type](tableName: String, name: String) extends Column[T]
+
+  object Arithmetic {
+    case class Multiplication(left: Column[Num], right: Column[Num]) extends Column[Num]
+
+    implicit class NumOps(self: Column[Num]) {
+      def *(other: Column[Num]) =
+        Multiplication(self, other)
+    }
+  }
+
+  object Aggregation {
+    case class Count(col: Column[Type]) extends Column[Num]
+    case class Sum(col: Column[Num]) extends Column[Num]
+    case class Max[T <: Type](col: Column[T]) extends Column[T]
+  }
+
+  object Literal {
+    case class Num(n: Double) extends Column[Type.Num]
+
+    implicit def toLiteralNum(n: Double): Num = Num(n)
+  }
+
+  object Usage {
+    val balance = SourceColumn[Num]("credit_cards", "balance")
+    val custId  = SourceColumn[Str]("credit_cards", "cust_id")
+
+    import Arithmetic._
+    import Aggregation._
+    import Literal._
+
+    val tripledCol = balance * 3.0      // Should compile.
+    val powerOfTwo = balance * balance
+
+    val maxNum = Max(balance * -1.0)
+    val maxCustId = Max(custId)
+//    val tripledStr = custId * 3       // Shouldn't compile.
+  }
+}
